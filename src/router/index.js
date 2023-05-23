fc34dc1d22653e197f1d9e4eb48f3d902c81c9b1
fpcoutinho <fpcoutinho5@gmail.com>
2023-05-23 09:22:05 -0300

    404 page on

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
+import NotFound from '../views/NotFound.vue'
 
 const routes = [
   {
@@ -12,6 +13,12 @@ const routes = [
     path: '/about',
     name: 'about',
     component: About,
+  },
+  //not found
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'not-found',
+    component: NotFound,
   },
 ]
 
---
+++ src/views/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <main>
+    <h1>404</h1>
+    <h2>Página não encontrada</h2>
+  </main>
+</template>
+
+<style scoped>
+h1 {
+  font-size: 10rem;
+  color: crimson;
+  text-align: center;
+  margin: 0;
+}
+</style>
